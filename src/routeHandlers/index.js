2bd38025f3fa83b56c39fbcb8574b4c2d527f727
Igor Barbashov <bis200705@gmail.com>
2022-06-19 23:35:19 +0300

    feature: add routes

---
+++ src/routeHandlers/index.js
@@ -0,0 +1,8 @@
+const {
+    getAllUsers, getUserById, updateUser,
+    createUser, deleteUser
+} = require('./user');
+
+module.exports = {
+    getAllUsers, getUserById, updateUser, createUser, deleteUser
+};
